d13aba61dd2b4243ae43a1dbf6753bec945f0857
helios0722 <90462652+helios0722@users.noreply.github.com>
2017-01-09 05:34:00 -0800

    Worked on Component - D:/Work/@/express-authentication-exercise/src/constants/index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,8 +1,1 @@
-require('dotenv').config();
-
-export const PORT = process.env.PORT || process.env.APP_PORT;
-export const DB = process.env.APP_DB;
-export const DOMAIN = process.env.APP_DOMAIN;
-export const SENDGRID_API_KEY = process.env.APP_SENDGRID_API;
-export const SECRET = process.env.APP_SECRET;
-export const HOST_EMAIL = process.env.APP_HOST_EMAIL;
+"Mon Jan 09 2017 05:34:00 GMT-0800 (Pacific Standard Time)" 
