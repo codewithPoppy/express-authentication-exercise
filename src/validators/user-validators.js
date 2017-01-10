33e82a34de769f695b39de8a7406346257a96be1
helios0722 <90462652+helios0722@users.noreply.github.com>
2017-01-10 11:23:00 -0800

    Component Updated - D:/Work/@/express-authentication-exercise/src/validators/user-validators.js

--- src/validators/user-validators.js
+++ src/validators/user-validators.js
@@ -1,12 +1,1 @@
-import { check } from 'express-validator';
-
-const name = check('name', 'Name is required').not().isEmpty();
-const username = check('username', 'Username is required').not().isEmpty();
-const email = check('email', 'Please Provide a valid email').isEmail();
-const password = check('password', 'Password is required').isLength({
-  min: 6,
-});
-
-export const RegisterValidations = [password, name, username, email];
-export const AuthenticateValidations = [username, password];
-export const ResetPassword = [email];
+"Tue Jan 10 2017 11:23:00 GMT-0800 (Pacific Standard Time)" 
