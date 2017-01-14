c71f91c16510b429353d33cd78747fdffe663270
helios0722 <90462652+helios0722@users.noreply.github.com>
2017-01-13 21:28:00 -0800

    Improved architecture - D:/Work/@/express-authentication-exercise/src/middlewares/validator-middleware.js

--- src/middlewares/validator-middleware.js
+++ src/middlewares/validator-middleware.js
@@ -1,15 +1,1 @@
-import { validationResult } from 'express-validator';
-
-// custom middleware
-// const customMiddleware = (req, res, next) => {}
-
-const validationMiddleware = (req, res, next) => {
-  let errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.json({
-      errors: errors.array(),
-    });
-  }
-  next();
-};
-export default validationMiddleware;
+"Fri Jan 13 2017 21:28:00 GMT-0800 (Pacific Standard Time)" 
